038d9c847d315bf4faedd869912a6378d417e163
Sophie Kirschner <pineapplemachine@gmail.com>
2020-06-06 16:37:23 +0300

    Improve documentation in capsule.core.heap

--- capsule/core/heap.d
+++ capsule/core/heap.d
@@ -2,45 +2,56 @@ module capsule.core.heap;
 
 public:
 
+/// Default comparison to use for determining the ordering or priority
+/// of items in a heap, when no other comparison function was specified.
 alias DefaultHeapCompare = (a, b) => (a < b);
 
 /// Implements a heap data structure, often used to represent a
 /// priority queue.
 /// https://github.com/python/cpython/blob/3.8/Lib/heapq.py#L140
 struct Heap(T, alias compare = DefaultHeapCompare) {
+    /// This array contains the elements in the heap.
     T[] elements;
     
     this(in size_t size) {
         this.reserve(size);
     }
     
+    /// Returns true when the heap is empty, false otherwise.
     bool empty() const {
         return this.elements.length == 0;
     }
     
+    /// Get the number of elements currently in the heap.
     size_t length() const {
         return this.elements.length;
     }
     
+    /// Reserve memory for some expected number of elements to be pushed.
     void reserve(in size_t size) {
         this.elements.reserve(size);
     }
     
+    /// Clear the heap.
     void clear() {
         this.elements.length = 0;
     }
     
+    /// Get the topmost element in the heap, without removing it.
     auto top() {
         assert(!this.empty);
         return this.elements[0];
     }
     
+    /// Push a new item onto the top of the heap.
+    /// Returns the index in the elements array where the new item ended up.
     size_t push(T item) {
         const length = this.elements.length;
         this.elements ~= item;
         return this.siftDown(0, length);
     }
     
+    /// Remove the topmost element of the heap and return its value.
     T pop() {
         assert(!this.empty);
         auto last = this.elements[$ - 1];
@@ -56,6 +67,7 @@ struct Heap(T, alias compare = DefaultHeapCompare) {
         }
     }
     
+    /// Remove and return the topmost element, and also push a new element.
     T replace(T item) {
         assert(!this.empty);
         auto top = this.elements[0];
@@ -64,6 +76,7 @@ struct Heap(T, alias compare = DefaultHeapCompare) {
         return top;
     }
     
+    /// Used internally to maintain the order of the elements array.
     size_t siftDown(in size_t start, in size_t index) {
         auto item = this.elements[index];
         size_t i = index;
@@ -82,6 +95,7 @@ struct Heap(T, alias compare = DefaultHeapCompare) {
         return i;
     }
     
+    /// Used internally to maintain the order of the elements array.
     size_t siftUp(in size_t index) {
         const size_t end = this.elements.length;
         size_t i = index;
