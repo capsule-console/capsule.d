2dcbcb61a96e080a876d8f2eb96228a9f46660d6
Sophie Kirschner <sophiek@pineapplemachine.com>
2020-06-27 13:18:08 +0300

    Improve test coverage in capsule.math.divceil

--- capsule/math/divceil.d
+++ capsule/math/divceil.d
@@ -19,10 +19,18 @@ T divceil(T)(in T dividend, in T divisor) pure nothrow @safe @nogc {
 
 /// Test coverage for divceil
 unittest {
+    assert(divceil(0, 1) == 0);
+    assert(divceil(0, 5000) == 0);
     assert(divceil(1, 1) == 1);
     assert(divceil(100, 1) == 100);
     assert(divceil(1, 2) == 1);
     assert(divceil(50, 2) == 25);
     assert(divceil(10, 4) == 3);
     assert(divceil(500, 30) == 17);
+    assert(divceil(1000, 10) == 100);
+    assert(divceil(1001, 10) == 101);
+    assert(divceil(1002, 10) == 101);
+    assert(divceil(1009, 10) == 101);
+    assert(divceil(1010, 10) == 101);
+    assert(divceil(1011, 10) == 102);
 }
